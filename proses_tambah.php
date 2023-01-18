89392a7d49dcd4bfc0a9af76175cd5640ba3b568
nandakhoerunissa <109189382+nandakhoerunissa@users.noreply.github.com>
2023-01-18 09:58:59 +0700

    Add files via upload

--- proses_tambah.php
+++ proses_tambah.php
@@ -7,9 +7,28 @@ if(isset($_POST['tambah_siswa'])){
     $tahun=$_POST['tahun'];
     $nominal=$_POST['nominal'];
     
-    $sql = "INSERT INTO tb_siswa(nama, kelas, nama_jurusan, tahun, nominal) VALUES ('$nama', '$kelas', '$nama_jurusan', '$tahun', '$nominal')";
+    $sql = "INSERT INTO tb_jurusan(nama_jurusan) VALUES ('$nama_jurusan')";
     $query = mysqli_query($koneksi, $sql);
-
+    
+    $sql = "SELECT max (id_jurusan)AS jurusan_id FROM tb_jurusan LIMIT 1";
+    $query = mysqli_query($koneksi, $sql);
+    
+    $data = mysqli_fetch_array ($squery);
+    $query = mysqli_query($koneksi, $sql);
+    
+    $sql = "INSERT INTO tb_spp(tahun, nominal) VALUES ('$tahun', '$nominal')";
+    $query = mysqli_query($koneksi, $sql);
+    
+    $sql = "SELECT max (id_spp)AS spp_id FROM tb_spp LIMIT 1";
+    $query = mysqli_query($koneksi, $sql);
+    
+    $data = mysqli_fetch_array ($squery);
+    $spp_id = $data ['spp_id'];
+    
+      
+    $sql = "INSERT INTO tb_siswa(nama,kelas) VALUES ('$nama','$kelas')";
+    $query = mysqli_query($koneksi, $sql);
+    
     if($query){
         header('location:join-tabel.php?status=sukses');
     }else{
